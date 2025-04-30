3bcc884d8b9253b1b3cdbbfd532a1ebe7ea0c66a
Paulodados <pmarciol@hotmail.com>
2025-04-29 21:45:25 -0300

    relatorio grafico2

--- gerar_grafico.py
+++ gerar_grafico.py
@@ -1,7 +1,13 @@
 import pandas as pd  
 import matplotlib.pyplot as plt  
 
-df = pd.read_csv('data.csv')  
-df['idade'].plot(kind='hist')  
+df = pd.read_csv('data.csv') 
+
+#criar figura e eixo explicitamente
+fig. ax = plt subplots()
+
+#associa o plot aos eixos criados
+df['idade'].plot(kind='hist' , ax=ax)
+ax  
 plt.title('Distribuição de Idades')  
 plt.savefig('grafico.png')  
